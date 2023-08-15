f3a9947bb1eb034063fcfc8d72e2bdb4e2953ef7
Rafael - RafilklDev <rafaelalmeidapenha@gmail.com>
2023-08-15 15:56:12 -0300

    Iniciando projeto de pedidoeletronico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,77 @@
-import Image from 'next/image'
+import { FormInput } from "@/components/formInput";
+import { FormLabel } from "@/components/formLabel";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-10 text-xs w-[800px]">
+      <div className="flex w-full bg-gray-200">
+        <img className="w-[180px] h-[170px] border-2 border-black border-b-0 border-r-0" src="https://www.dadinhooriginal.com.br/wp-content/uploads/2016/02/BONECO-COM-LOGO-1.png" />
+        <div className="flex-col w-full">
+          <div className="bg-gray-100 p-2 align-middle border-2 border-black text-red-700 font-bold">
+            DOCE SABOR INDÚSTRIA E COMÉRCIO DE PRODUTOS ALIMENTÍCIOS EIRELI</div>
+          <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t-0 font-bold">
+            CNPJ : 07.175.867/0002-08 - INSCRIÇÃO ESTADUAL :  143.154.297.110</div>
+          <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t-0">
+            Rua Monsenhor Abreu, 73 - Pari - São Paulo - SP - CEP: 03035-115</div>
+          <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t-0">
+            ADM DE VENDAS  (11) 2612-6270  Whastapp (11) 98365-0691</div>
+          <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t-0 border-b-0 text-red-700 font-semibold">
+            pedidos@dsabor.com.br ou pedidos@dadinhooriginal.com.br</div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full">
+        <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t- flex gap-2">
+          <div className="font-bold ">CÓDIGO:</div>
+          <div className="border-black border-r-2 pr-2">RESPOSTA</div>
+          <div className="font-bold">RAZÃO SOCIAL:</div>
+          <div className="break-normal">RESPOSTA</div>
+        </div>
+        <div className="bg-gray-100 p-2 w-full align-middle border-2 border-black border-t-0 flex gap-2">
+          <div className="font-bold">CNPJ:</div>
+          <div className="border-black border-r-2 pr-2">RESPOSTA</div>
+          <div className="font-bold">INSCR. EST:</div>
+          <div>RESPOSTA</div>
+        </div>
+        <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t-0 flex gap-2">
+          <div className="font-bold">RUA:</div>
+          <div className="w-full">RESPOSTA</div>
+        </div>
+        <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t-0 flex gap-2">
+          <div className="font-bold">BAIRRO:</div>
+          <div className="border-black border-r-2 pr-2 w-full">RESPOSTA</div>
+          <div className="font-bold">CIDADE:</div>
+          <div className=" border-black border-r-2 pr-2">RESPOSTA</div>
+          <div className="font-bold">ESTADO:</div>
+          <div className="">RESPOSTA</div>
+        </div>
+        <div className="bg-gray-100 p-2 align-middle border-2 border-black border-t-0 flex gap-2">
+          <div className="font-bold">CEP:</div>
+          <div className="border-black border-r-2 pr-2 w-1/4">RESPOSTA</div>
+          <div className="font-bold">TELEFONE:</div>
+          <div className=" border-black border-r-2 pr-2 w-1/4">RESPOSTA</div>
+          <div className="font-bold">TELEFONE:</div>
+          <div className=" border-black border-r-2 pr-2 w-1/4">RESPOSTA</div>
+          <div className="font-bold">Fax:</div>
+          <div className="w-1/4">RESPOSTA</div>
+        </div>
+        <div className="bg-gray-100 w-full p-2 align-middle border-2 border-black border-t-0 flex gap-2">
+          <div className="font-bold w-auto whitespace-nowrap">E-Mail:</div>
+          <div className="border-black border-r-2 pr-2 w-full">RESPOSTA</div>
+          <div className="font-bold w-auto whitespace-nowrap">N° SUFRAMA:</div>
+          <div className="w-full">RESPOSTA</div>
+        </div>
+        <div className="bg-gray-100 w-full p-2 align-middle border-2 border-black border-t-0 flex gap-2 flex-nowrap">
+          <div className="font-bold w-auto">Transporte:</div>
+          <div className="border-black border-r-2 pr-2 w-full">RESPOSTA</div>
+          <div className="font-bold w-auto">Telefone:</div>
+          <div className="w-full">RESPOSTA</div>
+        </div>
+        <div className="bg-gray-100 w-full p-2 align-middle border-2 border-black border-t-0 flex gap-2 flex-nowrap">
+          <div className="font-bold w-auto whitespace-nowrap">CONDIÇÃO DE PAGAMENTO:</div>
+          <div className="border-black border-r-2 pr-2 w-full">RESPOSTA</div>
+          <div className="font-bold w-auto whitespace-nowrap">Descontos em %:</div>
+          <div className="w-full">RESPOSTA</div>
+        </div>
       </div>
     </main>
   )
