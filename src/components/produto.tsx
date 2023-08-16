ed8255e6b1ca9c25fdebdd387dfc87a5bcb29894
Rafael - RafilklDev <rafaelalmeidapenha@gmail.com>
2023-08-16 14:25:36 -0300

    liberando o teste

--- src/components/produto.tsx
+++ src/components/produto.tsx
@@ -1,4 +1,4 @@
-export function Produto({ produto }: { produto: { produto: string, peso: string, preco: string } }) {
+export function Produto({ produto }: { produto: { produto: string, peso: string, preco: number } }) {
     return <div className="flex flex-col gap-0.5 items-center bg-gray-200 border-black border-2 px-10 py-2">
         <img className="w-20" src="https://www.dadinhooriginal.com.br/wp-content/uploads/2016/02/BONECO-COM-LOGO-1.png" />
         <div className="w-32 text-center">{produto?.produto}</div>
